963ab4518afa38ce9f7404d204a6fc9b4bc26142
sarajit <bdsarajit499@gamil.com>
2024-06-02 19:29:24 +0600

    column2 and column3 complete

---
+++ src/components/availableCamps/sortandSearch/SortAndSerchBar.jsx
@@ -0,0 +1,35 @@
+
+
+const SortAndSerchBar = ({layoutColumns,setLayoutColumns,handleSearch,handleSort}) => {
+ 
+
+  return (
+    <div className="p-6 bg-blue-100 my-10">
+      <div className="flex items-center space-x-4 mb-4 justify-center [&>*]:w-full">
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={handleSearch}
+          className="px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        />
+        <select
+          onChange={handleSort}
+          className="px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        >
+          <option value="">Sort By</option>
+          <option value="most-registered">Most Registered</option>
+          <option value="camp-fees">Camp Fees</option>
+          <option value="alphabetical">Alphabetical Order</option>
+        </select>
+        {/* layoutbutton */}
+        <button onClick={()=>setLayoutColumns(!layoutColumns)} className="hidden md:block px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 active:bg-blue-400 focus:ring-blue-500 focus:ring-opacity-50">
+          {
+            layoutColumns ? "Column 2": "Column 3"
+          }
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SortAndSerchBar
--- src/pages/AvailableCamps/AvailableCamps.jsx
+++ src/pages/AvailableCamps/AvailableCamps.jsx
@@ -2,23 +2,46 @@ import { useEffect, useState } from "react"
 import Container from "../../components/Shared/container/Container"
 import useAxiosCommon from "../../hooks/useAxiosCommon"
 import CampCard from "../../components/popularCamp/CampCard"
+import SortAndSerchBar from "../../components/availableCamps/sortandSearch/SortAndSerchBar"
 
 
 function AvailableCamps() {
 const axiosCommon = useAxiosCommon()
 const [availableCamps,setAvailableCamps] =useState([])
-const [sortingValue,setSortingValue] = useState()
+// search & sort 
+const [searchTerm, setSearchTerm] = useState('');
+const [sortOption, setSortOption] = useState('');
+const [layoutColumns,setLayoutColumns] = useState(true)
+//search & sort
 
 useEffect(async()=>{
 const {data} =await axiosCommon.get('/allavilableCamps');
 setAvailableCamps(data)
-},[])
+},[])              
+
+///handleSearch
+const handleSearch =(e) =>{
+  console.log(e.target.value)
+}
+//handleSort
+const handleSort =(e)=>{
+console.log(e.target.value)
+}
 console.log(availableCamps)
   return (
     <div>
       <div className="my-8">
       <h1 className="text-4xl text-center font-bold">Available Camps</h1>
-       <div className="grid grid-col-1 md:grid-cols-3 gap-10 mt-10">
+      {/* search and sort bar */}
+      <div>
+        <SortAndSerchBar 
+        setLayoutColumns={setLayoutColumns}
+        layoutColumns = {layoutColumns}
+        handleSort={handleSort}
+        handleSearch ={handleSearch}
+        ></SortAndSerchBar>
+      </div>
+       <div className={`grid grid-col-1  gap-10 mt-10 ${layoutColumns ? 'md:grid-cols-3':'md:grid-cols-2'}`}>
         {
              availableCamps && availableCamps.map(camp => <CampCard key={camp._id} popularCamp={camp}></CampCard>)
         }
