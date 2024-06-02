590f921fbed8ce00be53af339e9b1a7a9bfbb094
sarajit <bdsarajit499@gamil.com>
2024-06-02 08:13:47 +0600

    popular camp complete

---
+++ src/components/popularCamp/CampCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function CampCard({popularCamp}) {
+    const  {
+        CampName,
+        Image,
+        CampFees,
+        DateTime,
+        Location,
+        HealthcareProfessional,
+        ParticipantCount,
+    } = popularCamp || {};
+  return (
+    <div className="max-w-md mx-auto bg-blue-50 rounded-xl shadow-md overflow-hidden md:max-w-2xl my-6">
+      <div>
+        <div className="md:flex-shrink-0 ">
+          <img className="h-60 w-full object-cover:" src={Image} alt={CampName} />
+        </div>
+        <div className="p-8">
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{CampName}</div>
+          <p className="block mt-1 text-lg leading-tight font-medium text-black">{Location}</p>
+          <p className="mt-2 text-gray-500">{DateTime}</p>
+          <div className="mt-4">
+            <span className="text-gray-700 font-semibold">Fees: </span>
+            <span className="text-gray-500">{CampFees}</span>
+          </div>
+          <div className="mt-2">
+            <span className="text-gray-700 font-semibold">Healthcare Professional: </span>
+            <span className="text-gray-500">{HealthcareProfessional}</span>
+          </div>
+          <div className="mt-2">
+            <span className="text-gray-700 font-semibold">Participants: </span>
+            <span className="text-gray-500">{ParticipantCount}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CampCard
--- src/components/popularCamp/PopularCamp.jsx
+++ src/components/popularCamp/PopularCamp.jsx
@@ -1,13 +1,25 @@
 import useAllCamps from "../../hooks/useAllCamps"
 import Container from "../Shared/container/Container"
+import CampCard from "./CampCard"
 
 function PopularCamp() {
-  const {data} = useAllCamps()
-  console.log(data)
+  const {data : allCamp =[]} = useAllCamps()
+ 
+
+  const popularCamps = allCamp.filter((camp,inx)=> inx < 6 )
+  console.log(popularCamps)
   return (
     <div>
       <Container>
         <h1 className="text-4xl text-center font-bold">Our Popular Cmaps</h1>
+
+        {/* //popularCard */}
+        <div className="grid grid-col-1 md:grid-cols-3 gap-10 mt-10">
+          {
+           popularCamps && popularCamps.map(camp => <CampCard key={camp._id} popularCamp={camp}></CampCard>)
+          }
+        </div>
+        
       </Container>
     </div>
   )
