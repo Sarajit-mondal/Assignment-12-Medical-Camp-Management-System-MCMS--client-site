97e13f62ad6f98deabc4313ff9ff7fa8fdd518ac
sarajit <bdsarajit499@gamil.com>
2024-06-09 12:28:38 +0600

    registered camp delete befor payment

--- src/pages/Dashboard/userDashboard/UserRegisteredCamps.jsx
+++ src/pages/Dashboard/userDashboard/UserRegisteredCamps.jsx
@@ -2,19 +2,30 @@ import React from 'react'
 import CampParticipantsTable from '../../../components/dashboard/UserDashboard/CampParticipantsTable';
 import Heading from '../../../components/Shared/Heading';
 import useMyjoinCamp from '../../../hooks/useMyjoinCamp';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import toast from 'react-hot-toast';
 
 
 function UserRegisteredCamps() {
+  const axiosSecure = useAxiosSecure()
    const {data : participant = [],isLoading,refetch} = useMyjoinCamp()
     console.log(participant)
       const handleFeedback = (id) => {
         console.log(`Provide feedback for participant with id: ${id}`);
         // Implement feedback functionality
       };
-    
-      const handleCancel = (id) => {
+    //delete joined camp
+      const handleCancel = async(id) => {
         console.log(`Cancel participation for id: ${id}`);
         // Implement cancel functionality
+        try {
+         await axiosSecure.delete(`/deleteRegisteredCamps/${id}`)
+         toast.success("Delete Successfull")
+         refetch()
+        } catch (error) {
+          toast.error(error.message)
+          
+        }
       };
   return (
     <div className='space-y-4'>
